a215a32ef63c932fef9365e2f64a26b73ae045a2
bbbbruno <bbbbruno69@gmail.com>
2019-12-20 15:45:49 +0900

    change .rb to .yml in db dir

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-require_relative 'models/memo'
+require 'securerandom'
 
-enable :method_override
+require_relative 'models/memo'
 
 get '/' do
   @title = 'top'
@@ -13,41 +13,41 @@ get '/' do
   erb :index
 end
 
-get '/new/?' do
+get '/new' do
   @title = 'new'
   erb :new
 end
 
 post '/' do
-  data = { title: params[:title], content: params[:content], created_at: Time.now }
+  data = { title: params[:title], content: params[:content], created_at: Time.now, filename: SecureRandom.hex(5) }
   @memo = Memo.new(data)
   @memo.save
   redirect '/'
   erb :index
 end
 
-get '/:title/?' do |title|
+get '/:filename' do |filename|
   @title = 'show'
-  @memo = Memo.find(title)
+  @memo = Memo.find(filename)
   erb :show
 end
 
-get '/:title/edit/?' do |title|
+get '/:filename/edit' do |filename|
   @title = 'edit'
-  @memo = Memo.find(title)
+  @memo = Memo.find(filename)
   erb :edit
 end
 
-put '/:title/?' do |title|
-  @memo = Memo.find(title)
+put '/:filename' do |filename|
+  @memo = Memo.find(filename)
   @memo.title = params[:title]
   @memo.content = params[:content]
   @memo.save
   erb :show
 end
 
-delete '/:title/?' do |title|
-  Memo.destroy(title)
+delete '/:filename' do |filename|
+  Memo.destroy(filename)
   redirect '/'
   erb :index
 end
--- models/memo.rb
+++ models/memo.rb
@@ -1,36 +1,40 @@
 # frozen_string_literal: true
 
 require 'fileutils'
+require 'yaml'
 
 class Memo
-  attr_accessor :title, :content, :created_at
+  attr_accessor :title, :content, :created_at, :filename
 
-  def initialize(title: '', content: '', created_at:)
+  def initialize(title: '', content: '', created_at:, filename:)
     @title = title
     @content = content.gsub(/\R/, "\n")
     @created_at = created_at
+    @filename = filename
   end
 
   class << self
     def all
       Dir.open('db').each_child.with_object([]) do |f, a|
-        a << Memo.new(eval(File.open("db/#{f}").read))
+        data = YAML.load_file("db/#{f}")
+        a << new(data)
       end.sort_by(&:created_at)
+      rescue
+        retry
     end
 
-    def find(title)
-      data = eval(File.open("db/#{title}.rb").read)
+    def find(filename)
+      data = File.open("db/#{filename}.yml") { |f| YAML.load(f) }
       self.new(data)
     end
 
-    def destroy(title)
-      FileUtils.rm("db/#{title}.rb")
+    def destroy(filename)
+      FileUtils.rm("db/#{filename}.yml")
     end
   end
 
   def save
-    File.open("db/#{title}.rb", 'w') do |f|
-      f.print "{ title: \"#{title}\", content: \"#{content}\", created_at: \"#{created_at}\" }"
-    end
+    data = { title: title, content: content, created_at: created_at, filename: filename }
+    YAML.dump(data, File.open("db/#{filename}.yml", 'w'))
   end
 end
